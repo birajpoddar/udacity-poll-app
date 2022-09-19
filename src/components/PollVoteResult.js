08943bcce9a14dcfb0647ac52840623bac49e80c
Biraj Poddar <biraj.poddar@gmail.com>
2022-09-20 02:58:46 +0530

    Fix #13

--- src/components/PollVoteResult.js
+++ src/components/PollVoteResult.js
@@ -3,8 +3,13 @@ import { useParams } from 'react-router-dom';
 import PollingContainer from './presentation/PollingContainer';
 import PollVote from './PollVote';
 import PollResult from './PollResult';
+import PageNotFound from './presentation/PageNotFound';
 
 const PollVoteResult = (props) => {
+	if (props.id === null) {
+		return <PageNotFound />;
+	}
+
 	return (
 		<PollingContainer id={props.id}>
 			{props.vote === undefined ? (
@@ -19,6 +24,13 @@ const PollVoteResult = (props) => {
 export default connect(({ polls, authedUser }, props) => {
 	const { id } = useParams();
 	const poll = polls[id];
+
+	if (poll === undefined) {
+		return {
+			id: null,
+		};
+	}
+
 	const vote = authedUser.answers[id];
 
 	return {
